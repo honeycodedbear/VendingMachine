c96a2715460629b493158439db90b78dca90fb5f
Max Rogers <maxrogers90@gmail.com>
2015-02-28 17:36:34 -0500

    finishing up comments

--- slot.rb
+++ slot.rb
@@ -3,7 +3,7 @@ class Slot
   attr_accessor :items, :price
   def initialize(price)
     @items = [] #an array that holds the String name of each 'item'
-    @price = price 
+    @price = price
   end
   def depth
     @items.size
@@ -15,6 +15,7 @@ class Slot
   def sellItem
     #this pops off the first item and returns it
     @items.shift
+    #if you want it to be a front loading vending machine change .shift to .pop
   end
   def to_s #defining a new toString method so we can use it in the string Interpolation
     "Price: #{price}, Depth: #{depth}, Items: #{@items}"
--- vendingmachine.rb
+++ vendingmachine.rb
@@ -2,6 +2,8 @@ require './slot'
 
 class VendingMachine
   attr_accessor :slots, :item_count, :items_sold, :user_credit, :user_debt, :cash, :queue
+  #Initialize the Vendingmachine with a hash of the different slots and their prices and its starting cash amount
+  #Example VendingMachine.new({A1: 1, A2: 1, B1: 2, B2: 2}, 20)
   def initialize(slots, starting_cash)
     @cash = starting_cash
     #Already using a hash to initialize the grid why not use that as my data structure
@@ -14,6 +16,10 @@ class VendingMachine
     @queue = [] #holds the order queue
 
   end
+  #This method updates the Slot's stock adding them to the back of its queue. I imagined this as a back loading
+  #vending machine but if you want a front loading one then change the Slot.sellItem method from @items.shift to @items.pop.
+  #It will give you the desired result
+  #This accepts a hash {Slotname => ["itemnames","itemnames"]} or {Slotname: "itemname"}
   def add_load(new_load)
     new_load.each do |key,value|
       if value.class == [].class
@@ -33,11 +39,18 @@ class VendingMachine
     @slots.map{|key,value|"#{key}=> #{value},\n"}.join
   end
 
+  #Originally supported coins but noticed in the documentation you wanted no coin support.
+  #the to_i method forces it to be an integer
   def insert_credit(new_credit)
     @user_credit ||= 0
     @user_credit += new_credit.to_i
   end
 
+  #This method checks to see if the user has inserted enough credit to pay off their tab
+  #If they have inserted enough credit it will then check if there is enough internal cash to
+  #give them correct change
+  #then it will dispense the sold items from their respected slots
+  #It returns an array [[purchased, items, array], change]
   def despense
     if @user_credit >= @user_debt
       change = @user_credit - @user_debt
@@ -62,6 +75,7 @@ class VendingMachine
     end
   end
 
+  #This method checks to see if a selected slot exists or if its empty. If it is valid it will add it to the queue
   def selection(coord)
     coord = coord.to_sym if coord.class == "".class
     #^^ to make sure it is a symbol not a string
@@ -75,6 +89,9 @@ class VendingMachine
     end
   end
 
+  #This method searches through the slots for the first occurence of a desired item.
+  #If I had more time I would add every entry but this was a quick answer. It returns a hash
+  #{Slotname: firstOccuryingIndexOfItem, Slotname2: aDifferentOccuryingIndexOfItem}
   def findItem(item)
     result = {}
     @slots.each do |key, value|
